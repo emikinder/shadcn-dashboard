c2a6ef6c96f734dd114f5719bbb3139ef2761d7d
emikinder <emikinderk@gmail.com>
2025-03-17 13:42:15 -0300

    add accordion component and integrate it into the dashboard page

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,5 +1,42 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+const items = [
+  {
+    id: 'item-1',
+    title: 'Is it accessible?',
+    content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-2',
+    title: 'Is it accessible 2?',
+    content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-3',
+    title: 'Is it accessible 3?',
+    content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+];
+
 const Page = () => {
-  return <div>Accordion Page</div>;
+  return (
+    <Accordion type="single">
+      {items.map((item) => (
+        <AccordionItem
+          value={item.id}
+          key={item.id}
+        >
+          <AccordionTrigger>{item.title}</AccordionTrigger>
+          <AccordionContent>{item.content}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
 };
 
 export default Page;
